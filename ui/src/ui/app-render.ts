f58117111386e9f5c8571202d6096921a05e62ac
Vamsi Krishna Achyutuni <achyutunivk@gmail.com>
2026-02-04 13:03:07 -0500

    Import normalizeBasePath in app render

--- ui/src/ui/app-render.ts
+++ ui/src/ui/app-render.ts
@@ -52,7 +52,7 @@ import {
   updateSkillEnabled,
 } from "./controllers/skills.ts";
 import { icons } from "./icons.ts";
-import { TAB_GROUPS, subtitleForTab, titleForTab } from "./navigation.ts";
+import { normalizeBasePath, TAB_GROUPS, subtitleForTab, titleForTab } from "./navigation.ts";
 import { renderAgents } from "./views/agents.ts";
 import { renderChannels } from "./views/channels.ts";
 import { renderChat } from "./views/chat.ts";
